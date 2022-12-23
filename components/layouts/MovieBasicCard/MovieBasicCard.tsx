9f715c5b664510f1412d3f1af39f59f909c01304
berkaytek <berkaytek98@hotmail.com>
2022-12-23 21:47:53 +0300

    Added simple ui for MainPage

---
+++ components/layouts/MovieBasicCard/MovieBasicCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { QuestionMark } from '@mui/icons-material';
+
+interface IMovieBasicCardProps{
+  image?:string;
+  title?:string;
+}
+
+const MovieBasicCard: React.FC<IMovieBasicCardProps> = ({ image, title }) => {
+
+  return (
+    <Card style={{maxWidth:345, height:600}}>
+      <CardActionArea>
+        <CardMedia
+          style={{maxWidth:345, aspectRatio:3/5}}
+          component="img"
+          height="500"
+          width="300"
+          image={image == null ? "" : "https://image.tmdb.org/t/p/w300" + image }
+          title={title}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" style={{textAlign:'center'}}>
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default MovieBasicCard;
