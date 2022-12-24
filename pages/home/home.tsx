60fc1bb66abc1c95520ef71b85c58e2dc798b80a
berkaytek <berkaytek98@hotmail.com>
2022-12-24 14:38:37 +0300

    added couresel base

--- pages/home/home.tsx
+++ pages/home/home.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import service from '../../api/service';
 import { MovieModelBase } from '../../models/MovieModelBase';
-import { InferGetStaticPropsType } from 'next';
-import { Grid, makeStyles } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import MovieBasicCard from '../../components/layouts/MovieBasicCard/MovieBasicCard';
 
 
 export default function Home() {
   const [movieData,setMovieData] = React.useState<Array<MovieModelBase>>([]);
-
+  const [Carousel, setCarouselData] = React.useState<Array<MovieModelBase>>([]);
 
   async function getInitialData(){
     var initialMovieData:Array<MovieModelBase> = await service.getItems<MovieModelBase>("/api/Movies/GetMoviesByStartAndEndIndex?startIndex=0&endIndex=25")
     setMovieData(initialMovieData);
+  }
+
+  async function getInitialCorouselData(){
+    var startIndex = Math.floor(Math.random() * 100000);
+    var endIndex = startIndex + 5;
+    var initialCarouselData:Array<MovieModelBase> = await service.getItems<MovieModelBase>(`/api/Movies/GetMoviesByStartAndEndIndex?startIndex=${startIndex.toString()}&endIndex=${endIndex.toString()}`)
+    setCarouselData(initialCarouselData);
   }
 
   React.useEffect(()=>{
@@ -25,6 +28,7 @@ export default function Home() {
 
 
   return (
+    <Container>
     <Grid container spacing={3}>
         {movieData.map((data,index)=>
         <Grid key={index} item xs={12} sm={6} md={2}>
@@ -32,5 +36,6 @@ export default function Home() {
         </Grid>
           )}
     </Grid>
+    </Container>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 export default function Home() {
-  const [data,setData] = React.useState("");
-
-  async function getData(){
-    var result = await fetch("https://localhost:7071/WeatherForecast") 
-    var response = await result.text();
-    setData(response)
-  }
-
-  React.useEffect(()=>{
-    getData()
-  },[])
-
-
 
   return (
     <div>
-        {data}
+        Test
     </div>
   )
 }
