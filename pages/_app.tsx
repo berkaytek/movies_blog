36050723b28a0cdf3851f1703363e8d092351388
baytek <baytek@ford.com.tr>
2023-07-14 22:22:09 +0300

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import Layout from '@/components/Layout/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from "react";
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return( 
     <Layout>
     <Component {...pageProps} />
+    <Analytics />
     </Layout>
   )
 }
